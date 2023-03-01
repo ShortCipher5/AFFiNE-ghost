a373f73969fbe8dd77623fe2b790e85410c713a6
ShortCipher5 <4605025+ShortCipher5@users.noreply.github.com>
2023-03-01 11:44:38 +0800

    Wait for new page div

--- tests/libs/page-logic.ts
+++ tests/libs/page-logic.ts
@@ -1,7 +1,9 @@
 import type { Page } from '@playwright/test';
 
 export async function newPage(page: Page) {
-  return page.getByText('New Page').click();
+  //return page.getByText('New Page').click();
+  const newPageButton = await page.waitForSelector('div:has-text("New Page")');
+  await newPageButton.click();
 }
 
 export async function clickPageMoreActions(page: Page) {
